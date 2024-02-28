0580295637b6f8530d98a4cfc8a484dcd982d534
Denis16543 <158287693+Denis16543@users.noreply.github.com>
2024-02-28 13:58:01 +0200

    jgjgj

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
     width: 1920,
-    height: 1080,
+    height:1080,
     parent: game,
     physics: {
         default: 'arcade',
@@ -17,16 +17,21 @@ var config = {
     }
 };
 var game = new Phaser.Game(config);
+var worldWidth = 9600;
 
 function preload() {
     this.load.image('fon', 'assets/fon.png');
-    
+    this.load.image('fon 1','assets/fon 1.jpg');
+
+
 }
 function create() {
    
     this.add.image(0, 0, 'fon').setOrigin(0,0);
-
+    this.add.image(0, 0, 'fon').setOrigin(0,0);
+    this.add.tileSprite(0, 0, worldWidth, 1080, "fon 1").setOrigin(0, 0);
 }
+
 
 
 
